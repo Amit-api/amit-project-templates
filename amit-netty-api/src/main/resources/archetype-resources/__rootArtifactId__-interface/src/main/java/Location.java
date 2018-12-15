f88635b29af9919c51a37e7f8a36e590fca08f65
Alexandru Motriuc <motriuc@yahoo.com>
2018-12-15 20:41:22 +1100

    create package

---
+++ amit-netty-api/src/main/resources/archetype-resources/__rootArtifactId__-interface/src/main/java/Location.java
@@ -0,0 +1,9 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+/**
+ * class used for resource location 
+ */
+public class Location {
+}
---
+++ amit-netty-api/src/main/resources/archetype-resources/__rootArtifactId__-service/src/main/java/service/__serviceName__Imp.java
@@ -0,0 +1,28 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.service;
+
+import java.util.concurrent.CompletableFuture;
+
+import com.test.api.test_api.Person;
+
+import ${package}.InvalidIdException;
+import ${package}.Data;
+import ${package}.intrf.${serviceName};
+
+/**
+ * a simple service implementation 
+ */
+public class ${serviceName}Imp implements ${serviceName} {
+	@Override
+	public CompletableFuture<Void> createPerson(Person person) {
+		return CompletableFuture.completedFuture(null);
+	}
+
+	@Override
+	public CompletableFuture<Person> getPerson(Integer personId) {
+		return CompletableFuture.completedFuture(new Person().withPersonId(
+				personId).withPersonName("sasha"));
+	}	
+}
